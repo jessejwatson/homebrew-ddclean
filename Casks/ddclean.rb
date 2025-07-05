f7f07a475b9a28d511c0d9ecd36b69cf79a65a6c
Jesse Watson <95267462+jessejwatson@users.noreply.github.com>
2025-07-05 15:53:57 +1000

    Update ddclean.rb url

--- Casks/ddclean.rb
+++ Casks/ddclean.rb
@@ -2,7 +2,7 @@ cask "ddclean" do
   version "0.1.0"
   sha256 "d7fc75c37958df4cc1b7638d86c727760f8ac0142312344dd7d0d9d0e83aa3ad"
 
-  url "https://github.com/jessejwatson/ddclean/releases/download/v#{version}/DDClean.zip"
+  url "https://github.com/jessejwatson/ddclean-public/releases/download/v#{version}/DDClean.zip"
   name "DDClean"
   desc "Automate file system organization"
   homepage "https://github.com/jessejwatson/ddclean"
