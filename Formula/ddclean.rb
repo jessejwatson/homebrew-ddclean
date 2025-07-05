e95727f2bef4ddd07490bde9be3bc304d7560586
Jesse Watson <95267462+jessejwatson@users.noreply.github.com>
2025-07-05 15:37:43 +1000

    Create ddclean.rb

---
+++ Formula/ddclean.rb
@@ -0,0 +1,14 @@
+class Ddclean < Formula
+  desc "Automate file system organization"
+  homepage "https://github.com/jessejwatson/ddclean"
+  version "0.1.0"
+  
+  url "https://github.com/jessejwatson/ddclean-cli/releases/download/v0.1.0/ddclean-cli.zip"
+  sha256 "1e43bab8073d48a7247f165f143d245af19e19a24ab21e5e4f19161c88ba8974"
+  
+  depends_on "python@3.13.5"
+
+  def install
+    bin.install "main.py" => "ddclean"
+  end
+end
